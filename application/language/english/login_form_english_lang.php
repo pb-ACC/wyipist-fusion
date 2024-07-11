eeda0ef5de2cfba90afc7dd519ddd6d5ffd41b8d
pbrito <1210123@isep.ipp.pt>
2024-07-11 15:30:13 +0100

    Criação do ficheiro login_form_english_lang.php

---
+++ application/language/english/login_form_english_lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: André
+ * Date: 28-09-2015
+ * Time: 11:35
+ */
+
+$lang['form_name_value'] = 'Username';
+$lang['form_pass_value'] = 'Password';
+$lang['login'] = 'Login';
+$lang['error_required'] = 'Fill in this field please.';
+$lang['error_login'] = 'Wrong username or password.';
