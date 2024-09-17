5656e6052ff5d60b38bf1507b1a7230fe9696da7
pbrito <1210123@isep.ipp.pt>
2024-09-17 16:10:53 +0100

    Ajustes e correções

--- application/controllers/standards/ListarFiltro.php
+++ application/controllers/standards/ListarFiltro.php
@@ -271,7 +271,7 @@ class ListarFiltro extends CI_Controller
 
                 $this->load->model('standards/stocks/GetPalets');
                 //$setor = '\''.$newSector.'\'';             
-                $setor = '\'ST008\', \'ST009\', \'ST009A\', \'ST009B\', \'ST009C\', \'ST009D\', \'ST009E\', \'ST010\', \'ST011\', \'ST012\', \'ST013\', \'ST014\', \'ST015\', \'ST016\', \'ST020\', \'ST200\', \'ST201\', \'ST202\', \'ST203\', \'ST204\', \'ST205\', \'ST206\', \'ST207\', \'ST290\'';               
+                $setor = '\'ST008\', \'ST009\', \'ST009A\', \'ST009B\', \'ST009C\', \'ST009D\', \'ST009E\', \'ST011\', \'ST012\', \'ST013\', \'ST014\', \'ST015\', \'ST016\', \'ST020\', \'ST200\', \'ST201\', \'ST202\', \'ST203\', \'ST204\', \'ST205\', \'ST206\', \'ST207\', \'ST290\'';               
                 $paletes=$this->GetPalets->armazem($setor,'>0');                
 
                 $this->load->model('standards/others/Buttons');
@@ -332,7 +332,7 @@ class ListarFiltro extends CI_Controller
 
                 $this->load->model('standards/stocks/GetPalets');
                 //$setor = '\''.$newSector.'\'';             
-                $setor = '\'ST008\', \'ST009\', \'ST009A\', \'ST009B\', \'ST009C\', \'ST009D\', \'ST009E\', \'ST010\', \'ST011\', \'ST012\', \'ST013\', \'ST014\', \'ST015\', \'ST016\', \'ST020\', \'ST200\', \'ST201\', \'ST202\', \'ST203\', \'ST204\', \'ST205\', \'ST206\', \'ST207\', \'ST290\'';               
+                $setor = '\'ST008\', \'ST009\', \'ST009A\', \'ST009B\', \'ST009C\', \'ST009D\', \'ST009E\', \'ST011\', \'ST012\', \'ST013\', \'ST014\', \'ST015\', \'ST016\', \'ST020\', \'ST200\', \'ST201\', \'ST202\', \'ST203\', \'ST204\', \'ST205\', \'ST206\', \'ST207\', \'ST290\'';               
                 $paletes=$this->GetPalets->armazem($setor,'<0');
 
                 $this->load->model('standards/others/Buttons');
@@ -393,7 +393,7 @@ class ListarFiltro extends CI_Controller
 
                 $this->load->model('standards/stocks/GetPalets');
                 //$setor = '\''.$newSector.'\'';             
-                $setor = '\'ST008\', \'ST009\', \'ST009A\', \'ST009B\', \'ST009C\', \'ST009D\', \'ST009E\', \'ST010\', \'ST011\', \'ST012\', \'ST013\', \'ST014\', \'ST015\', \'ST016\', \'ST020\', \'ST200\', \'ST201\', \'ST202\', \'ST203\', \'ST204\', \'ST205\', \'ST206\', \'ST207\', \'ST290\'';               
+                $setor = '\'ST008\', \'ST009\', \'ST009A\', \'ST009B\', \'ST009C\', \'ST009D\', \'ST009E\', \'ST011\', \'ST012\', \'ST013\', \'ST014\', \'ST015\', \'ST016\', \'ST020\', \'ST200\', \'ST201\', \'ST202\', \'ST203\', \'ST204\', \'ST205\', \'ST206\', \'ST207\', \'ST290\'';               
                 $paletes=$this->GetPalets->armazem($setor,'=0');
 
                 $this->load->model('standards/others/Buttons');
