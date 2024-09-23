2081380988068c10d8d222ec7b7421d97ef9b24f
pbrito <1210123@isep.ipp.pt>
2024-09-23 17:47:38 +0100

    Correções aos setores para movimentar

--- application/controllers/standards/ListarFiltro.php
+++ application/controllers/standards/ListarFiltro.php
@@ -490,11 +490,12 @@ class ListarFiltro extends CI_Controller
             
             $emp = strtoupper($emp);
             if($emp == 'CERAGNI'){                
-                $empresa = '\''.$emp.'\'';
+                $empresa = '\''.$emp.'\'';                
                // echo $empresa;
+               $setor = '\'ST008\', \'ST009\', \'ST009A\', \'ST009B\', \'ST009C\', \'ST009D\', \'ST009E\', \'ST011\', \'ST012\', \'ST013\', \'ST014\', \'ST015\', \'ST016\', \'ST020\', \'ST200\', \'ST201\', \'ST202\', \'ST203\', \'ST204\', \'ST205\', \'ST206\', \'ST207\', \'ST290\'';               
 
                 $this->load->model('standards/others/GetZonas');                
-                $zonas=$this->GetZonas->zonaCelula($empresa,$emp);
+                $zonas=$this->GetZonas->zonaCelula($empresa,$emp,$setor);
                 
                 $this->load->model('standards/stocks/GetReferences');
                 $refs=$this->GetReferences->referencias($emp);                                
@@ -506,9 +507,10 @@ class ListarFiltro extends CI_Controller
                 
             }else{
                 $empresa = '\''.$emp.'\'';
+                $setor = '\'FB003\', \'CL001\''; 
                 
                 $this->load->model('standards/others/GetZonas');                
-                $zonas=$this->GetZonas->zonaCelula($empresa,$emp);
+                $zonas=$this->GetZonas->zonaCelula($empresa,$emp,$setor);
 
                 $this->load->model('standards/stocks/GetReferences');
                 $refs=$this->GetReferences->referencias($emp);                                
